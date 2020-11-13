24c63e24374b654224d6ff8017147e8e99a5bc4e
Draku Quay <vantayden@gmail.com>
2020-11-13 14:32:48 +0700

    initial commit

---
+++ app/src/main/java/com/asia/libexample/MainActivity.java
@@ -0,0 +1,14 @@
+package com.asia.libexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
